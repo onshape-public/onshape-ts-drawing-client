2b594a6be4dbbfde0a8a925e6961ed394b76316c
Robert Miner <rminer@ptc.com>
2024-04-05 07:02:15 -0400

    Create inspection symbols in one request, offset from dimension text

--- create-inspection-symbols.ts
+++ create-inspection-symbols.ts
@@ -13,10 +13,10 @@ try {
 
   const apiClient = await ApiClient.createApiClient(drawingScriptArgs.stackToUse);
   let viewToUse: View2 = null;
-  let textPosition: number[] = null;
+  let symbolPosition: number[] = null;
   let parentAnnotationLogicalId: string = null;
   let annotationsInView: Annotation[] = null;
-  let symbolPosition: number[] = null;
+  let annotationsToRequest: Object[] = null;
 
   /**
    * Retrieve a drawing view and some of its edges to get enough information to create the inspection symbol
@@ -34,70 +34,82 @@ try {
   }
 
   if (viewToUse != null && annotationsInView !== null) {
-
-    try {
-      for (let indexAnnotation = 0; indexAnnotation < annotationsInView.length; indexAnnotation++) {
-        let annotation: Annotation = annotationsInView[indexAnnotation];
-        if (annotation.type === AnnotationType.RADIAL_DIMENSION) {
-          textPosition = annotation.radialDimension.textPosition.coordinate;
-          parentAnnotationLogicalId = annotation.radialDimension.logicalId;
-        } else if (annotation.type === AnnotationType.DIMENSION_POINT_TO_POINT_LINEAR) {
-          textPosition = annotation.pointToPointDimension.textPosition.coordinate;
-          parentAnnotationLogicalId = annotation.pointToPointDimension.logicalId;
-        } else {
-          textPosition = null;
-          parentAnnotationLogicalId = null;
-        }
-  
-        if (textPosition !== null && parentAnnotationLogicalId !== null) {
-          // Create request to create an inspection symbol on the annotation
-          const modifyRequest = await apiClient.post(`api/v6/drawings/d/${drawingScriptArgs.documentId}/w/${drawingScriptArgs.workspaceId}/e/${drawingScriptArgs.elementId}/modify`,  {
-            description: "Add Inspection Symbol",
-            jsonRequests: [ {
-              messageName: 'onshapeCreateAnnotations',
-              formatVersion: '2021-01-01',
-              annotations: [
-                {
-                  type: 'Onshape::InspectionSymbol',
-                  inspectionSymbol: {
-                    borderShape: 'Circle',
-                    borderSize: 2,
-                    parentAnnotation: parentAnnotationLogicalId,
-                    parentLineIndex: 0.0,
-                    position: {
-                      coordinate: textPosition,
-                      type: 'Onshape::Reference::Point'
-                    },
-                    textHeight: 0.12
-                  }
-                }
-              ]
-            }]
-          }) as BasicNode;
-    
-          LOG.info('Initiated creation of inspection symbol in drawing', modifyRequest);
-          let jobStatus: ModifyJob = { requestState: 'ACTIVE', id: '' };
-          const end = timeSpan();
-          while (jobStatus.requestState === 'ACTIVE') {
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-            const elapsedSeconds = end.seconds();
-    
-            // If modify takes over 1 minute, then log and continue
-            if (elapsedSeconds > 60) {
-              LOG.error(`Inspection symbol creation timed out after ${elapsedSeconds} seconds`);
-              break;
-            }
-    
-            LOG.debug(`Waited for modify seconds=${elapsedSeconds}`);
-            jobStatus = await apiClient.get(`api/drawings/modify/status/${modifyRequest.id}`) as ModifyJob;
-          }
-        }
+    // Create the message to create inspection symbols for multiple dimensions in view
+    for (let indexAnnotation = 0; indexAnnotation < annotationsInView.length; indexAnnotation++) {
+      let annotation: Annotation = annotationsInView[indexAnnotation];
+      if (annotation.type === AnnotationType.RADIAL_DIMENSION) {
+        symbolPosition = annotation.radialDimension.textPosition.coordinate;
+        symbolPosition[0] += 0.5;
+        parentAnnotationLogicalId = annotation.radialDimension.logicalId;
+      } else if (annotation.type === AnnotationType.DIMENSION_POINT_TO_POINT_LINEAR) {
+        symbolPosition = annotation.pointToPointDimension.textPosition.coordinate;
+        symbolPosition[0] += 0.5;
+        parentAnnotationLogicalId = annotation.pointToPointDimension.logicalId;
+      } else {
+        symbolPosition = null;
+        parentAnnotationLogicalId = null;
       }
 
-      LOG.info(`Created inspection symbols`);
-    } catch (error) {
-      console.error(error);
-      LOG.error('Create inspection symbol failed in modify API call', error);
+      if (symbolPosition !== null && parentAnnotationLogicalId !== null) {
+        let annotationRequest = {
+          type: 'Onshape::InspectionSymbol',
+          inspectionSymbol: {
+            borderShape: 'Circle',
+            borderSize: 2,
+            parentAnnotation: parentAnnotationLogicalId,
+            parentLineIndex: 0.0,
+            position: {
+              coordinate: symbolPosition,
+              type: 'Onshape::Reference::Point'
+            },
+            textHeight: 0.12
+          }
+        }
+        if (annotationsToRequest === null) {
+          annotationsToRequest = [ annotationRequest ];
+        } else {
+          annotationsToRequest.push(annotationRequest);
+        }
+      }
+    }     
+      
+    if (annotationsToRequest !== null) {
+      try {
+        // Create request to create an inspection symbol on the annotation
+        const modifyRequest = await apiClient.post(`api/v6/drawings/d/${drawingScriptArgs.documentId}/w/${drawingScriptArgs.workspaceId}/e/${drawingScriptArgs.elementId}/modify`,  {
+          description: "Add Inspection Symbol",
+          jsonRequests: [ {
+            messageName: 'onshapeCreateAnnotations',
+            formatVersion: '2021-01-01',
+            annotations: annotationsToRequest
+          }]
+        }) as BasicNode;
+  
+        LOG.info('Initiated creation of inspection symbol in drawing', modifyRequest);
+        let jobStatus: ModifyJob = { requestState: 'ACTIVE', id: '' };
+        const end = timeSpan();
+        while (jobStatus.requestState === 'ACTIVE') {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+          const elapsedSeconds = end.seconds();
+  
+          // If modify takes over 1 minute, then log and continue
+          if (elapsedSeconds > 60) {
+            LOG.error(`Inspection symbol creation timed out after ${elapsedSeconds} seconds`);
+            break;
+          }
+  
+          LOG.debug(`Waited for modify seconds=${elapsedSeconds}`);
+          jobStatus = await apiClient.get(`api/drawings/modify/status/${modifyRequest.id}`) as ModifyJob;
+        }       
+
+        LOG.info(`Created inspection symbols`);
+      } catch (error) {
+        console.error(error);
+        LOG.error('Create inspection symbol failed in modify API call', error);
+      }
+    } else {
+      console.log('No dimensions found for which to create the inspection symbols.');
+      LOG.error('Create inspection symbols failed due to no dimensions found.');  
     }
   } else {
     console.log('Insufficient view and annotation information to create the inspection symbols.');
